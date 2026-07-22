be6f927023a76fe9c849fcc9a78caa4cc25d6bd4
Paulo688 <goncalvesgabriel3598@gmail.com>
2026-07-22 16:12:24 -0300

    Add files via upload
    Adição de todos os arquivos do CodeConnect.

---
+++ scripts.js
@@ -0,0 +1,157 @@
+const uploadBtn = document.getElementById("upload-btn");
+const inputUpload = document.getElementById("image-upload");
+
+uploadBtn.addEventListener("click", () =>
+{
+    inputUpload.click();
+})
+
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => 
+    {
+        const leitor = new FileReader();
+
+        leitor.onload = () => 
+        {
+            resolve({ url: leitor.result, nome: arquivo.name });
+        };
+
+        leitor.onerror = () => 
+        {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`);
+        };
+        leitor.readAsDataURL(arquivo);
+    });
+}
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p");
+
+inputUpload.addEventListener("change", async (evento) =>
+{
+    const arquivo = evento.target.files[0];
+
+    if (arquivo)
+    {
+        try {
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
+            imagemPrincipal.src = conteudoDoArquivo.url;
+            nomeDaImagem.textContent = conteudoDoArquivo.nome;
+        } catch (erro) 
+        {
+            console.error("Erro na leitura do arquivo");
+        }
+    }
+})
+
+const inputTags = document.getElementById("input-tags");
+const listaTags = document.getElementById("lista-tags");
+
+listaTags.addEventListener("click", (evento) =>
+{
+    if (evento.target.classList.contains("remove-tag"))
+    {
+        const tagQueQueremosRemover = evento.target.parentElement;
+        listaTags.removeChild(tagQueQueremosRemover);
+    }
+})
+
+const tagsDisponiveis = ["Front-end", "Programação", "Data Science", "Full-stack", "HTML", "CSS", "JavaScript"];
+
+async function verificaTagsDisponiveis(tagTexto)
+{
+    return new Promise((resolve) =>
+    {
+        setTimeout(() =>
+        {
+            resolve(tagsDisponiveis.includes(tagTexto));
+        }, 10)
+    })
+}
+
+inputTags.addEventListener("keypress", async (evento) =>
+{
+    if (evento.key === "Enter")
+    {
+        evento.preventDefault();
+        const tagTexto = inputTags.value.trim();
+        if (tagTexto !== "")
+        {
+            try 
+            {
+            const tagExiste = await verificaTagsDisponiveis(tagTexto);
+            if (tagExiste)
+            {
+            const tagNova = document.createElement("li");
+            tagNova.innerHTML = `<p>${tagTexto}<p> <img src="./img/close-black.svg" class="remove-tag"</p>`;
+            listaTags.appendChild(tagNova);
+            inputTags.value = "";
+            }
+            else
+            {
+                inputTags.value = "";
+                alert("Tag não encontrada.");
+            }
+            } catch (error)
+            {
+                console.error("Erro ao verificar a existência da tag");
+                alert("Erro ao verificar a existência da tag. Verifique o console");
+            }
+        }
+    }
+})
+
+const botaoPublicar = document.querySelector(".botao-publicar");
+
+async function publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto)
+{
+    return new Promise((resolve, reject) => 
+    {
+        setTimeout(() =>
+        {
+            const deuCerto = Math.random() > 0.1;
+
+            if (deuCerto)
+            {
+                resolve("Projeto publicado com sucesso")
+            }
+            else
+            {
+                reject("Erro ao publicar o projeto.");
+            }
+        }, 500)
+    })
+}
+
+botaoPublicar.addEventListener("click", async (evento) =>
+{
+    evento.preventDefault();
+
+    const nomeDoProjeto = document.getElementById("nome").value;
+    const descricaoDoProjeto = document.getElementById("descricao").value;
+    const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
+
+    try
+    {
+        const resultado = await  publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto);
+        console.log(resultado);
+        alert("Deu tudo certo, publicação realizada com sucesso!");
+    } catch (error) {
+        console.log("Deu errado:", error);
+        alert("Algo deu errado!");
+    }
+})
+
+const botaoDescartar = document.querySelector(".botao-descartar");
+
+botaoDescartar.addEventListener("click", (evento) =>
+{
+    evento.preventDefault();
+
+    const formulario = document.querySelector("form");
+    formulario.reset();
+
+    imagemPrincipal.src = "./img/imagem1.png";
+    nomeDaImagem.textContent = "image_projeto.png";
+    listaTags.innerHTML = "";
+})
